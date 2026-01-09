22436f692a17efcd2f60685fde562c047b4f8aa2
KhanhDai484 <138193926+KhanhDai484@users.noreply.github.com>
2026-01-10 01:33:44 +0700

    feat: add file application-test.yml for test DB

--- src/test/java/com/glassystem/optics/OpticsManagementApplicationTests.java
+++ src/test/java/com/glassystem/optics/OpticsManagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.glassystem.optics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional("test")
 class OpticsManagementApplicationTests {
 
 	@Test
