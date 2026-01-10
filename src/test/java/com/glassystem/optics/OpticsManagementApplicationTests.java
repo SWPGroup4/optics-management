34925fe93aa2460c65055aa88dc906219680767d
KhanhDai484 <138193926+KhanhDai484@users.noreply.github.com>
2026-01-11 01:44:45 +0700

    feat: setup direct deployment to EC2 via GitHub Actions

--- src/test/java/com/glassystem/optics/OpticsManagementApplicationTests.java
+++ src/test/java/com/glassystem/optics/OpticsManagementApplicationTests.java
@@ -2,13 +2,9 @@ package com.glassystem.optics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.NONE
-)
-@ActiveProfiles("test")
+@SpringBootTest
 class OpticsManagementApplicationTests {
 
 	@Test
