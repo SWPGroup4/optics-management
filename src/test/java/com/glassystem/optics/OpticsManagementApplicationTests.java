89deb7d6ca415ffa2798d967eaa53f33a57d94f6
KhanhDai484 <138193926+KhanhDai484@users.noreply.github.com>
2026-01-10 19:13:54 +0700

    fix: activate test profile & fix contextLoads

--- src/test/java/com/glassystem/optics/OpticsManagementApplicationTests.java
+++ src/test/java/com/glassystem/optics/OpticsManagementApplicationTests.java
@@ -2,10 +2,11 @@ package com.glassystem.optics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Transactional("test")
+@ActiveProfiles("test")
 class OpticsManagementApplicationTests {
 
 	@Test
