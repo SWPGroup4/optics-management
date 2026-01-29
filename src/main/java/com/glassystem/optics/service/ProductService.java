5e35e9524927bf9596ab1abfdc18b94f4fd8df23
KhanhDai484 <138193926+KhanhDai484@users.noreply.github.com>
2026-01-29 08:38:20 +0700

    fix: product image & payment controller

--- src/main/java/com/glassystem/optics/service/ProductService.java
+++ src/main/java/com/glassystem/optics/service/ProductService.java
@@ -12,10 +12,13 @@ import com.glassystem.optics.dto.request.ProductUpsertRequest;
 import com.glassystem.optics.dto.response.ProductImageResponse;
 import com.glassystem.optics.dto.response.ProductResponse;
 import com.glassystem.optics.entity.Product;
+import com.glassystem.optics.entity.ProductImage;
 import com.glassystem.optics.enums.ProductStatus;
+import com.glassystem.optics.enums.S3ImageName;
 import com.glassystem.optics.exception.AppException;
 import com.glassystem.optics.exception.ErrorCode;
 import com.glassystem.optics.mapper.ProductMapper;
+import com.glassystem.optics.repository.ProductImageRepository;
 import com.glassystem.optics.repository.ProductRepository;
 import com.glassystem.optics.specification.ProductSpecifications;
 import lombok.AccessLevel;
@@ -36,42 +39,44 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ProductService {
-	ProductRepository productRepository;
-	ProductMapper productMapper;
+    ProductRepository productRepository;
+    ProductMapper productMapper;
+    FileStorageService fileStorageService;
+    ProductImageRepository productImageRepository;
 
-	public ProductResponse create(ProductCreateRequest request) {
-		Product product = productMapper.toProduct(request);
-		product = productRepository.save(product);
-		return productMapper.toProductResponse(product);
-	}
+    public ProductResponse create(ProductCreateRequest request) {
+        Product product = productMapper.toProduct(request);
+        product = productRepository.save(product);
+        return productMapper.toProductResponse(product);
+    }
 
-	public ProductResponse getById(String id) {
-		Product product = productRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND));
-		return productMapper.toProductResponse(product);
-	}
+    public ProductResponse getById(String id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND));
+        return productMapper.toProductResponse(product);
+    }
 
-	public ProductResponse update(String id, ProductUpsertRequest request) {
-		Product product = productRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND));
-		productMapper.updateProduct(product, request);
-		product = productRepository.save(product);
-		return productMapper.toProductResponse(product);
-	}
+    public ProductResponse update(String id, ProductUpsertRequest request) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.PRODUCT_NOT_FOUND));
+        productMapper.updateProduct(product, request);
+        product = productRepository.save(product);
+        return productMapper.toProductResponse(product);
+    }
 
-	public void delete(String id) {
-		if (!productRepository.existsById(id)) {
-			throw new AppException(ErrorCode.PRODUCT_NOT_FOUND);
-		}
-		productRepository.deleteById(id);
-	}
+    public void delete(String id) {
+        if (!productRepository.existsById(id)) {
+            throw new AppException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+        productRepository.deleteById(id);
+    }
 
 
-	public List<ProductResponse> getProducts() {
-		return  productRepository.findAll().stream().map(productMapper::toProductResponse).toList();
+    public List<ProductResponse> getProducts() {
+        return  productRepository.findAll().stream().map(productMapper::toProductResponse).toList();
     }
 
     @Transactional
     public List<ProductImageResponse> uploadProductImages(String productId,
-            List<MultipartFile> files
+                                                          List<MultipartFile> files
     ) throws IOException {
 
         Product product = productRepository.findById(productId)
