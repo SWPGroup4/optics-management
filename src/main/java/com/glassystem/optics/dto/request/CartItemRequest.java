ea7661425ecf1fa9286bb6d6e58e6bf378bcba54
nguyensytuan0811 <nguyensytuan2005118@gmail.com>
2026-02-27 23:43:07 +0700

    order combo done

--- src/main/java/com/glassystem/optics/dto/request/CartItemRequest.java
+++ src/main/java/com/glassystem/optics/dto/request/CartItemRequest.java
@@ -1,5 +1,8 @@
 package com.glassystem.optics.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -15,6 +18,10 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CartItemRequest {
+	@NotBlank(message = "FIELD_MISSING")
 	String skuId;
+
+	@NotNull(message = "INVALID_QUANTITY")
+	@Min(value = 1, message = "INVALID_QUANTITY")
 	Integer quantity;
 }
