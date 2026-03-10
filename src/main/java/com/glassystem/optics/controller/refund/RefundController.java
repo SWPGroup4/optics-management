91b5f789ef81ff53925122c412c59a5a1384b963
KhanhDai484 <138193926+KhanhDai484@users.noreply.github.com>
2026-03-10 11:33:34 +0700

    feat: set role

--- src/main/java/com/glassystem/optics/controller/refund/RefundController.java
+++ src/main/java/com/glassystem/optics/controller/refund/RefundController.java
@@ -6,6 +6,7 @@ import com.glassystem.optics.dto.response.ApiResponse;
 import com.glassystem.optics.dto.response.RefundResponse;
 import com.glassystem.optics.service.RefundService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/refund")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('MANAGER') or hasRole('ADMIN')")
 public class RefundController {
 
     private final RefundService refundService;
