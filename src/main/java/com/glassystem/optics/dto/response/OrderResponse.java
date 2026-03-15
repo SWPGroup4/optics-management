7aec9c02ae16a2faafecfde3013d33e201e4a49e
KhanhDai484 <138193926+KhanhDai484@users.noreply.github.com>
2026-03-16 01:16:23 +0700

    fix: api dasboard

--- src/main/java/com/glassystem/optics/dto/response/OrderResponse.java
+++ src/main/java/com/glassystem/optics/dto/response/OrderResponse.java
@@ -1,7 +1,8 @@
 package com.glassystem.optics.dto.response;
 
-import com.glassystem.optics.enums.OrderItemType;
 import com.glassystem.optics.enums.OrderStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,7 +22,9 @@ public class OrderResponse {
     String deliveryAddress;
     String phoneNumber;
 
+
     OrderStatus orderStatus;
+
     BigDecimal totalAmount;
     BigDecimal depositAmount;
     List<OrderItemResponse> items;
